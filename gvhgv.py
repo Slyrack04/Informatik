5c265099c5aa61ecb398791f54e968d5e7592963
Slyrack04 <131754668+Slyrack04@users.noreply.github.com>
2023-04-26 08:18:18 +0200

    ijh

--- gvhgv.py
+++ gvhgv.py
@@ -1,1 +1,15 @@
-print(haha)
+x = int(input())
+y = int(input())
+r = str(input())
+if r == "*":
+	print(x,r,y,"=")
+	print(x*y)
+elif r == "-":
+	print(x,r,y,"=")
+	print(x-y)
+elif r == "+":
+	print(x,r,y,"=")
+	print(x+y)
+else:
+	print(x,r,y,"=") 
+	print(x/y)
